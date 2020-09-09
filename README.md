34bb69c81ed3166ed480bbb0015b6efb2e6880be
MajesticRev <63527030+MajesticRev@users.noreply.github.com>
2020-09-09 02:07:16 -0500

    Updating Files
    I got the website working completely, now i just need to add some more effects for winning and losing and I'll be done.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # rps.github.io
 This is just me learning javascript through a rock, paper, scissors. AKA my hello world which im publishing more to show progress.
-Thanks for checking it out and check out my twitter @asworo5
+Thanks for checking it out. If you've been interested or are interested in web developement (calling what im doing web developement is genourous, but still) try it out. This is easy to learn and fun!
--- app.js
+++ app.js
@@ -1,14 +1,75 @@
-// Cacheing the dom
-const userScore = 0;
-const computerScore = 0;
+// declaring vairables and cacheing the dom
+let userScore = 0;
+let compScore = 0;
 const userScore_span = document.getElementById("user-score");
 const compScore_span = document.getElementById("comp-score");
 const scoreBoard_div = document.querySelector(".score-board")
-const result_div = document.querySelector(".result")
+const result_div = document.querySelector(".result > p")
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
 
-rock_div.addEventListener('click', function() {
-    console.log("hey you clicked on rock... dumbass LMAO");
-})
+//declaring functions to ditermine outcomes
+function getCompChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function win(userChoice, compChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = compScore;
+    result_div.innerHTML = userChoice + " beats " + compChoice + ". You win!"
+}
+
+function lose(userChoice, compChoice) {
+    compScore++;
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = compScore;
+    result_div.innerHTML = userChoice + " loses to " + compChoice + ". Try Again!"
+}
+
+function draw(userChoice, compChoice) {
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = compScore;
+    result_div.innerHTML = userChoice + " ties with " + compChoice + ". Try Again!"
+}
+
+function game(userChoice){
+    const compChoice = getCompChoice();
+    switch (userChoice + compChoice) {
+        case "scissorspaper":
+        case "paperrock":
+        case "rockscissors":
+            win(userChoice, compChoice);
+            break;
+        case "scissorsrock":
+        case "paperscissors":
+        case "rockpaper":
+            lose(userChoice, compChoice);
+            break;
+        case "scissorsscissors":
+        case "paperpaper":
+        case "rockrock":
+            draw(userChoice, compChoice);
+            break;
+    }
+}
+
+//Actually putting the different outputs against eachother
+function main() {
+    rock_div.addEventListener('click', function() {
+        game("rock");
+    })
+
+    paper_div.addEventListener('click', function() {
+        game("paper");
+    })
+
+    scissors_div.addEventListener('click', function() {
+        game("scissors");
+    })
+}
+
+main();
